4444a0b430b6746680d39643a06f91707ba4cbb8
Alexandre <alexandrejeronimo2001@gmail.com>
2021-04-30 19:26:12 +0100

    responsive fix

--- public/layout/footer.php
+++ public/layout/footer.php
@@ -1,5 +1,5 @@
       <footer class="flex items-center justify-between flex-shrink-0 p-4 border-t max-h-14">
-        <div>Hotel Inteligente</div>
+        <div>Smart Hotel</div>
         <div class="text-sm">
           Feito por:
           <a class="text-blue-400 underline" href="" target="_blank" rel="noopener noreferrer">Alexandre Jerónimo</a>
