f9aa8636d5e97816177e5871762e72802a9ded29
imdrk5 <79916095+imdrk5@users.noreply.github.com>
2021-04-30 12:50:35 +0100

    PHP do Login e Atualização do CSS

--- public/index.php
+++ public/index.php
@@ -1,3 +1,29 @@
+<?php
+$users = array(
+  array("admin", "admin"),
+  array("regular", "regular")
+);
+
+$login = "none";
+
+if (isset($_POST['username']) && isset($_POST['password'])) {
+  foreach ($users as $user) {
+    if ($_POST['username'] == $user[0] && $_POST['password'] == $user[1]) {
+      $login = "done";
+      echo "Credenciais corretas";
+      echo "Username: " . $_POST['username'] . "<br>";
+      echo "Password: " . $_POST['password'] . "<br>";
+      session_start();
+      $_SESSION["username"] = $_POST['username'];
+      $_SESSION["password"] = $_POST['password'];
+      header("Location: dashboard");
+    } else {
+      $login = "failed";
+    }
+  }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -12,21 +38,23 @@
 
 <body>
   <div class="bg-image-login"></div>
-  <form action="index.php" method="post">
+  <form action="" method="post">
     <div class="flex flex-col justify-center min-h-screen sm:py-12">
       <div class="p-10 mx-auto xs:p-0 md:w-full md:max-w-md">
+        <?php
+        if ($login == "failed") {
+          echo "<div id='divAlerta' class='bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded' role='alert'>
+          <strong class='font-bold'>Erro!</strong>
+          <span class='block sm:inline'>O nome de usuário e a senha fornecidos não correspondem às informações em nossos registros. Verifique-as e tente novamente.</span></div>";
+        }
+        ?>
         <div class="w-full bg-white divide-y divide-gray-200 rounded-lg shadow">
           <div class="px-5 py-7">
-            <label class="block pb-1 text-sm font-semibold text-gray-600">E-mail</label>
-            <input type="text" class="w-full px-3 py-2 mt-1 mb-5 text-sm border rounded-lg" />
+            <label class="block pb-1 text-sm font-semibold text-gray-600">Username</label>
+            <input type="text" id="inUsername" class="w-full px-3 py-2 mt-1 mb-5 text-sm border rounded-lg" name="username" required />
             <label class="block pb-1 text-sm font-semibold text-gray-600">Password</label>
-            <input type="text" class="w-full px-3 py-2 mt-1 mb-5 text-sm border rounded-lg" />
-            <button type="button" class="transition duration-200 bg-blue-500 hover:bg-blue-600 focus:bg-blue-700 focus:shadow-sm focus:ring-4 focus:ring-blue-500 focus:ring-opacity-50 text-white w-full py-2.5 rounded-lg text-sm shadow-sm hover:shadow-md font-semibold text-center inline-block">
-              <span class="inline-block mr-2">Login</span>
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="inline-block w-4 h-4">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8l4 4m0 0l-4 4m4-4H3" />
-              </svg>
-            </button>
+            <input type="text" id="inPassword" class="w-full px-3 py-2 mt-1 mb-5 text-sm border rounded-lg" name="password" required />
+            <input type="submit" value="Login" class="transition duration-200 bg-blue-500 hover:bg-blue-600 focus:bg-blue-700 focus:shadow-sm focus:ring-4 focus:ring-blue-500 focus:ring-opacity-50 text-white w-full py-2.5 rounded-lg text-sm shadow-sm hover:shadow-md font-semibold text-center inline-block"></input>
           </div>
         </div>
       </div>
