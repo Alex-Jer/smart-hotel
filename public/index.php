3c04f9d5da8f9e5d2f8587a63d558740cb27513d
Alexandre <alexandrejeronimo2001@gmail.com>
2021-04-30 15:14:09 +0100

    pasta views e js, update ao .htaccess

--- public/index.php
+++ public/index.php
@@ -31,8 +31,8 @@ if (isset($_POST['username']) && isset($_POST['password'])) {
   <meta charset="UTF-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link rel="stylesheet" href="css/tailwind.css" />
-  <link rel="stylesheet" href="css/styles.css" />
+  <link rel="stylesheet" href="/public/css/tailwind.css" />
+  <link rel="stylesheet" href="/public/css/styles.css" />
   <title>Hotel Inteligente</title>
 </head>
 
@@ -43,7 +43,7 @@ if (isset($_POST['username']) && isset($_POST['password'])) {
       <div class="p-10 mx-auto xs:p-0 md:w-full md:max-w-md">
         <?php
         if ($login == "failed") {
-          echo "<div id='divAlerta' class='bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded' role='alert'>
+          echo "<div id='divAlerta' class='px-4 py-3 text-red-700 bg-red-100 border border-red-400 rounded' role='alert'>
           <strong class='font-bold'>Erro!</strong>
           <span class='block sm:inline'>O nome de usuário e a senha fornecidos não correspondem às informações em nossos registros. Verifique-as e tente novamente.</span></div>";
         }
@@ -51,10 +51,10 @@ if (isset($_POST['username']) && isset($_POST['password'])) {
         <div class="w-full bg-white divide-y divide-gray-200 rounded-lg shadow">
           <div class="px-5 py-7">
             <label class="block pb-1 text-sm font-semibold text-gray-600">Username</label>
-            <input type="text" id="inUsername" class="w-full px-3 py-2 mt-1 mb-5 text-sm border rounded-lg" name="username" required />
+            <input type="text" id="inUsername" class="w-full px-3 py-2 mt-1 mb-5 text-sm border rounded-lg" name="username" required autofocus />
             <label class="block pb-1 text-sm font-semibold text-gray-600">Password</label>
             <input type="text" id="inPassword" class="w-full px-3 py-2 mt-1 mb-5 text-sm border rounded-lg" name="password" required />
-            <input type="submit" value="Login" class="transition duration-200 bg-blue-500 hover:bg-blue-600 focus:bg-blue-700 focus:shadow-sm focus:ring-4 focus:ring-blue-500 focus:ring-opacity-50 text-white w-full py-2.5 rounded-lg text-sm shadow-sm hover:shadow-md font-semibold text-center inline-block"></input>
+            <input type="submit" value="Login" class="transition cursor-pointer duration-200 bg-blue-500 hover:bg-blue-600 focus:bg-blue-700 focus:shadow-sm focus:ring-4 focus:ring-blue-500 focus:ring-opacity-50 text-white w-full py-2.5 rounded-lg text-sm shadow-sm hover:shadow-md font-semibold text-center inline-block"></input>
           </div>
         </div>
       </div>
