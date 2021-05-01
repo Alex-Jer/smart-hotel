dbeb2f72964e64f099b38ceec6ff7aec2af220b5
Alexandre <alexandrejeronimo2001@gmail.com>
2021-05-01 12:13:17 +0100

    update à estrutura das diretorias

--- public/views/table.php
+++ public/views/table.php
@@ -6,8 +6,8 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <!-- Styles & Tailwind -->
-  <link rel="stylesheet" href="css/tailwind.css" />
-  <link rel="stylesheet" href="css/styles.css" />
+  <link rel="stylesheet" href="/public/css/tailwind.css" />
+  <link rel="stylesheet" href="/public/css/styles.css" />
   <title>Tabela - Hotel Inteligente</title>
 </head>
 
