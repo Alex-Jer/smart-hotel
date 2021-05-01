db795446fc54006b35e42a092d2f6a7438ce8a21
imdrk5 <79916095+imdrk5@users.noreply.github.com>
2021-05-01 16:55:07 +0100

    Update sidebar.php

--- public/views/templates/sidebar.php
+++ public/views/templates/sidebar.php
@@ -12,7 +12,6 @@
         <!-- sidebar header -->
         <div class="flex items-center justify-between flex-shrink-0 p-2" :class="{'lg:justify-center': !isSidebarOpen}">
           <span class="p-2 text-xl font-semibold leading-8 tracking-wider uppercase whitespace-nowrap">
-            <span :class="{'lg:hidden': isSidebarOpen}">SH</span>
             <span :class="{'lg:hidden': !isSidebarOpen}">Smart Hotel</span>
           </span>
           <button @click="toggleSidbarMenu()" class="p-2 rounded-md lg:hidden">
