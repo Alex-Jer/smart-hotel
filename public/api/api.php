83fd8e7cc6ca70ee02e47b40afdb1f62698cb372
Alexandre <alexandrejeronimo2001@gmail.com>
2021-05-02 19:57:28 +0100

    API - GET

--- public/api/api.php
+++ public/api/api.php
@@ -4,41 +4,65 @@ header('Content-Type: text/html; charset=utf-8');
 $method_type = $_SERVER['REQUEST_METHOD'];
 
 if ($method_type == 'POST') {
-  echo 'recebido um post' . PHP_EOL;
-  print_r($_POST);
-  if (isset($_POST['region'])) {
-    echo "Region: {$_POST['region']}" . PHP_EOL;
-    switch ($_POST['region']) {
+    echo 'recebido um post' . PHP_EOL;
+    print_r($_POST);
+    if (isset($_POST['region'])) {
+        echo "Region: {$_POST['region']}" . PHP_EOL;
+        switch ($_POST['region']) {
       case 'rooms':
         if (isset($_POST['number']) && isset($_POST['sensor']) && isset($_POST['value']) && isset($_POST['time'])) {
-          echo file_put_contents("data/{$_POST['region']}/{$_POST['number']}/{$_POST['sensor']}/value.txt", "$_POST[time]" . ';' . "$_POST[value]");
-          echo file_put_contents("data/{$_POST['region']}/{$_POST['number']}/{$_POST['sensor']}/log.txt", "$_POST[time]" . ';' . "$_POST[value]" . PHP_EOL, FILE_APPEND);
+            echo file_put_contents("data/{$_POST['region']}/{$_POST['number']}/{$_POST['sensor']}/value.txt", "$_POST[time]" . ';' . "$_POST[value]");
+            echo file_put_contents("data/{$_POST['region']}/{$_POST['number']}/{$_POST['sensor']}/log.txt", "$_POST[time]" . ';' . "$_POST[value]" . PHP_EOL, FILE_APPEND);
         } else {
-          echo 'ERRO: Dados em Falta!' . PHP_EOL;
+            echo 'ERRO: Dados em Falta!' . PHP_EOL;
         }
         break;
       case 'pool':
       case 'parking':
       case 'rooftop':
         if (isset($_POST['value']) && isset($_POST['time']) && isset($_POST['sensor'])) {
-          echo file_put_contents("data/{$_POST['region']}/{$_POST['sensor']}/value.txt", "$_POST[time]" . ';' . "$_POST[value]");
-          echo file_put_contents("data/{$_POST['region']}/{$_POST['sensor']}/log.txt", "$_POST[time]" . ';' . "$_POST[value]" . PHP_EOL, FILE_APPEND);
+            echo file_put_contents("data/{$_POST['region']}/{$_POST['sensor']}/value.txt", "$_POST[time]" . ';' . "$_POST[value]");
+            echo file_put_contents("data/{$_POST['region']}/{$_POST['sensor']}/log.txt", "$_POST[time]" . ';' . "$_POST[value]" . PHP_EOL, FILE_APPEND);
         } else {
-          echo 'ERRO: Dados em Falta!' . PHP_EOL;
+            echo 'ERRO: Dados em Falta!' . PHP_EOL;
         }
         break;
       default:
         echo 'ERRO: A região indicada não existe!' . PHP_EOL;
         break;
     }
-  } else {
-    echo 'ERRO: Parâmetros inválidos' . PHP_EOL;
-  }
+    } else {
+        echo 'ERRO: Parâmetros inválidos' . PHP_EOL;
+    }
 } elseif ($method_type == 'GET') {
-  echo 'recebido um get' . PHP_EOL;
-  if (isset($_GET['nome'])) {
-    echo file_get_contents("data/{$_GET['nome']}/temperature/value.txt");
-  }
+    echo 'recebido um get' . PHP_EOL;
+
+    if (isset($_GET['region']) && isset($_GET['type'])) {
+        switch ($_GET['region']) {
+          case 'rooms':
+            echo 'Region: Rooms' . PHP_EOL;
+            if (isset($_GET['number'])) { // Ex: api?region=rooms&number=1&type=log
+                echo file_get_contents("data/rooms/{$_GET['number']}/door/{$_GET['type']}.txt") . PHP_EOL;
+                echo file_get_contents("data/rooms/{$_GET['number']}/temperature/{$_GET['type']}.txt") . PHP_EOL;
+            } else {
+                echo 'ERRO: Dados em Falta!' . PHP_EOL;
+            }
+            break;
+          case 'pool':
+            echo 'Region: Pool' . PHP_EOL;
+            echo file_get_contents("data/pool/temperature/{$_GET['type']}.txt");
+            break;
+          case 'parking':
+            echo 'Region: Parking' . PHP_EOL;
+            break;
+          case 'rooftop':
+            echo 'Region: Rooftop' . PHP_EOL;
+            break;
+          default:
+            echo 'ERRO: A região indicada não existe!' . PHP_EOL;
+            break;
+        }
+    }
 } else {
-  echo 'ERRO: Método não permitido!';
+    echo 'ERRO: Método não permitido!';
 }
--- public/views/templates/sidebar.php
+++ public/views/templates/sidebar.php
@@ -42,10 +42,21 @@
                     </path>
                   </svg>
                 </span>
-                <span class="sidebar-item">Histórico</span>
+                <span class="sidebar-item">Quartos</span> <!-- Temporário -->
               </a>
             </li>
-            <!-- Sidebar Links... -->
+            <li>
+              <a href="logs" class="flex items-center p-2 space-x-2 rounded-md dark:hover:bg-dark hover:bg-gray-100">
+                <span>
+                  <svg class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="#9ca3af">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                      d="M7 21h10a2 2 0 002-2V9.414a1 1 0 00-.293-.707l-5.414-5.414A1 1 0 0012.586 3H7a2 2 0 00-2 2v14a2 2 0 002 2z">
+                    </path>
+                  </svg>
+                </span>
+                <span class="sidebar-item">Piscina</span> <!-- Temporário -->
+              </a>
+            </li>
           </ul>
         </nav>
 
